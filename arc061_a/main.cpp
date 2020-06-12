d850bf57594ae4e9012b50cddcbccec3f9435616
ohsuyuki <papu8osu@gmail.com>
2020-06-13 00:27:12 +0900

    Initial Commit

---
+++ arc061_a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  arc061_a
+//
+//  Created by yuuki oosu on 2020/06/13.
+//  Copyright © 2020 yuuki oosu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
